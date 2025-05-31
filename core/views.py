911d1ed4f5e55bd6c96041c0b73745254473e89a
JuanEPB <JuanEPB20@outlook.com>
2025-05-30 18:12:10 -0600

    first commit

---
+++ core/views.py
@@ -0,0 +1,208 @@
+import requests
+from django.shortcuts import render, redirect
+
+def lista_medicamentos(request):
+    token = request.session.get("jwt")
+    if not token:
+        return redirect("login")
+
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
+    try:
+        response = requests.get("http://localhost:3000/api/medicamentos/all", headers=headers)
+        if response.status_code == 200:
+            medicamentos = response.json()
+        else:
+            medicamentos = []
+    except requests.RequestException:
+        medicamentos = []
+
+    return render(request, 'core/medicamentos.html', {'medicamentos': medicamentos})
+
+# vista de login
+def login_view(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        contraseña = request.POST.get("contraseña")
+
+        try:
+            response = requests.post("http://localhost:3000/api/auth/login", json={
+                "email": email,
+                "contraseña": contraseña
+            })
+
+            if response.status_code == 201:
+                token = response.json().get("access_token")
+                request.session["jwt"] = token
+                return redirect("medicamentos")
+            else:
+                return render(request, "core/login.html", {"error": "Credenciales incorrectas."})
+        
+        except requests.RequestException:
+            return render(request, "core/login.html", {"error": "Error al conectar con la API."})
+
+    return render(request, "core/login.html")
+
+def logout_view(request):
+    request.session.flush()
+    return redirect("login")
+
+# vista de inventario
+def inventory_view(request):
+    token = request.session.get("jwt")
+    if not token:
+        return redirect("login")
+
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
+    try:
+        response = requests.get("http://localhost:3000/api/medicamentos/all", headers=headers)
+        if response.status_code == 200:
+            inventory = response.json()
+        else:
+            inventory = []
+    except requests.RequestException:
+        inventory = []
+
+    return render(request, 'core/inventory.html', {'inventory': inventory})
+
+# vista de reportes
+def report_view(request):
+    token = request.session.get("jwt")
+    if not token:
+        return redirect("login")
+
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
+    try:
+        response = requests.get("http://localhost:3000/api/medicamentos/all", headers=headers)
+        if response.status_code == 200:
+            reports = response.json()
+        else:
+            reports = []
+    except requests.RequestException:
+        reports = []
+
+    return render(request, 'core/reports.html', {'reports': reports})
+
+# vista de users
+def user_view(request):
+    token = request.session.get("jwt")
+    if not token:
+        return redirect("login")
+
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
+    try:
+        response = requests.get("http://localhost:3000/api/medicamentos/all", headers=headers)
+        if response.status_code == 200:
+            users = response.json()
+        else:
+            users = []
+    except requests.RequestException:
+        users = []
+
+    return render(request, 'core/users.html', {'users': users})
+
+# vista de pedidos
+def order_view(request):
+    token = request.session.get("jwt")
+    if not token:
+        return redirect("login")
+
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
+    try:
+        response = requests.get("http://localhost:3000/api/medicamentos/all", headers=headers)
+        if response.status_code == 200:
+            orders = response.json()
+        else:
+            orders = []
+    except requests.RequestException:
+        orders = []
+
+    return render(request, 'core/orders.html', {'orders': orders})
+
+# vista de proveedores
+def supplier_view(request):
+    token = request.session.get("jwt")
+    if not token:
+        return redirect("login")
+
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
+    try:
+        response = requests.get("http://localhost:3000/api/medicamentos/all", headers=headers)
+        if response.status_code == 200:
+            supplier = response.json()
+        else:
+            supplier = []
+    except requests.RequestException:
+        supplier = []
+
+    return render(request, 'core/supplier.html', {'supplier': supplier})
+
+# vista de configuracion
+def settings_view(request):
+    token = request.session.get("jwt")
+    if not token:
+        return redirect("login")
+
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
+    try:
+        response = requests.get("http://localhost:3000/api/medicamentos/all", headers=headers)
+        if response.status_code == 200:
+            settings = response.json()
+        else:
+            settings = []
+    except requests.RequestException:
+        settings = []
+
+    return render(request, 'core/settings.html', {'settings': settings})
+
+
+# vista de crear medicamento
+def create_medicamento_view(request):
+    token = request.session.get("jwt")
+    if not token:
+        return redirect("login")
+
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }   
+    if request.method == "POST":
+        nombre = request.POST.get("nombre")
+        descripcion = request.POST.get("descripcion")
+        precio = request.POST.get("precio")
+        cantidad = request.POST.get("cantidad")
+
+        try:
+            response = requests.post("http://localhost:3000/api/medicamentos/create", json={
+                "nombre": nombre,
+                "descripcion": descripcion,
+                "precio": precio,
+                "cantidad": cantidad
+            }, headers=headers)
+
+            if response.status_code == 201:
+                return redirect("medicamentos")
+            else:
+                return render(request, "core/create_medicamento.html", {"error": "Error al crear el medicamento."})
+        
+        except requests.RequestException:
+            return render(request, "core/create_medicamento.html", {"error": "Error al conectar con la API."})
---
+++ pharmacontrol_django/urls.py
@@ -0,0 +1,33 @@
+"""
+URL configuration for pharmacontrol_django project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from core.views import lista_medicamentos, login_view, logout_view, inventory_view, report_view, user_view, order_view, supplier_view       
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('medicamentos/', lista_medicamentos, name='medicamentos'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('inventory/', inventory_view, name='inventory'),
+    path('report/', report_view, name='reports'),
+    path('user/', user_view, name='users'),
+    path('order/', order_view, name='orders'),
+    path('supplier/', supplier_view, name='suppliers'),
+    path('settings/', inventory_view, name='settings'),
+
+]
